95e811dd4cfd1a5cf2d18f9d7eae603978b196b1
Ronny A. Bruhin <ronny.bruhin+github@hotmail.com>
2025-10-24 09:40:39 +0200

    added codecov vite plugin and setup codecov

--- ReactRustTodo/frontend/vite.config.ts
+++ ReactRustTodo/frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "<bundle project name>",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 })
